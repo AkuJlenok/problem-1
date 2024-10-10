9ee2e9d7738194df8fdc2cdcc696a41455515d60
Kirill Begaliev <st129977@student.spbu.ru>
2024-10-10 23:06:39 +0300

    Now program saves file into array and reverses it

--- Assignment2a/Array.cpp
+++ Assignment2a/Array.cpp
@@ -9,9 +9,23 @@ Gets file, read its size, create array with needed size and puts file in this ar
 
 int Array ()
 {
+    //opens file and reads its size
     std::ifstream infile;
     infile.open("filetoread",std::ios::binary|std::ios::in);
     int file_size = std::filesystem::file_size("filetoread");
-    std::cout<< file_size <<std::endl;
-    return 0;
+    
+    //arrays to save and reverse file
+    char* file = new char [file_size];
+    char* reversed_file = new char [file_size];
+    
+    //saves file into array and reverses into another one
+    infile.read(file,sizeof(file));
+    for (int i = 0; i < file_size; ++i)
+    {
+        reversed_file[i] = file[file_size-i];
+    }
+    delete [] file; 
+    
+    delete [] reversed_file;
+    return 0;   
 }
