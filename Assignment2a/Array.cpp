405752898cedb9850cc55eb31704af9749bed8a9
Kirill Begaliev <st129977@student.spbu.ru>
2024-10-10 23:25:16 +0300

    Now writes the reversed array into the new file, final version of project

--- Assignment2a/Array.cpp
+++ Assignment2a/Array.cpp
@@ -19,13 +19,19 @@ int Array ()
     char* reversed_file = new char [file_size];
     
     //saves file into array and reverses into another one
-    infile.read(file,sizeof(file));
+    infile.read(file,file_size);
     for (int i = 0; i < file_size; ++i)
     {
         reversed_file[i] = file[file_size-i];
     }
-    delete [] file; 
+    delete [] file;
+    infile.close();
     
+    //writes reversed array into the new file
+    std::ofstream outfile;
+    outfile.open("answer_file",std::ios::binary|std::ios::out);
+    outfile.write(reversed_file,file_size);
     delete [] reversed_file;
+    
     return 0;   
 }
