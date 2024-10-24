b06950de2e142096826527e727a0ade4ff255377
Kirill Begaliev <st129977@student.spbu.ru>
2024-10-24 23:56:12 +0300

    second fix

--- Assignment3/Transformer.cpp
+++ Assignment3/Transformer.cpp
@@ -1,3 +1,48 @@
 /* Kirill Begaliev st129977@student.spbu.ru
 Transfromer class definition
 */
+
+#include <iostream>
+#include "Transformer.hpp"
+
+Transformer :: Transformer()
+{
+    health = 1000;
+    size = 100;
+    strength = 150;
+}
+
+Transformer :: ~Transformer()
+{
+    std::cout<< "Transformer was destroyed" <<std::endl;
+}
+
+unsigned Transformer :: get_health()
+{
+    return health;
+}
+
+unsigned Transformer :: get_size()
+{
+    return size;
+}
+
+unsigned Transformer :: get_strength()
+{
+    return strength;
+}
+
+void Transformer :: set_health(unsigned new_health)
+{
+    health = new_health;
+}
+
+void Transformer :: set_size(unsigned new_size)
+{
+    size = new_size;
+}
+
+void Transformer :: set_strength(unsigned new_strength)
+{
+    strength = new_strength;
+}
--- Assignment3/Transfromer.cpp
+++
@@ -1,8 +0,0 @@
-/* Kirill Begaliev st129977@student.spbu.ru
-Transfromer class definition
-*/
-
-#include <iostream>
-#include "Transformer.hpp"
-
-
