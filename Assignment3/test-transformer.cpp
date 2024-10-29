6e6198e5867ac66c3e08c04cd8f12ca4f0182ad5
Kirill Begaliev <st129977@student.spbu.ru>
2024-10-29 03:06:05 +0300

    Added subclass Zoomorphs(Transformers that transform into the animals) and modified tests

--- Assignment3/test-transformer.cpp
+++ Assignment3/test-transformer.cpp
@@ -4,7 +4,7 @@ tests for all classes
 
 #include <gtest/gtest.h>
 #include <iostream>
-#include "Transformer.hpp"
+#include "Zoomorph.hpp"
 
 TEST(Transformers_tests, default_constructor)
 {
@@ -34,7 +34,66 @@ TEST(Transformers_tests, constructor)
     EXPECT_EQ(Zolo.is_transformer_speak_transformers_language(), true);
     EXPECT_EQ(Zolo.is_transformer_speak_humans_language(), false);
 }
-//in Transformers_tests already checks weapon's constructors 
+//in Transformers_tests already checked get methods of Transformer, Weapon and Communicate_module classes and their constructors
+
+TEST(Transformers_tests, set_methods)
+{
+    Transformer Zolo;
+    Zolo.set_health(900);
+    Zolo.set_size(120);
+    Zolo.set_strength(9999);
+    EXPECT_EQ(Zolo.get_health(),900);
+    EXPECT_EQ(Zolo.get_size(),120);
+    EXPECT_EQ(Zolo.get_strength(),9999);
+}
+
+TEST(Weapon_tests, set_methods)
+{
+    Weapon axe(40,2,true,false);
+    axe.set_damage(100);
+    axe.set_cooldown(1);
+    EXPECT_EQ(axe.get_damage(),100);
+    EXPECT_EQ(axe.get_cooldown(),1);
+    EXPECT_EQ(axe.is_weapon_melee(), true);
+    EXPECT_EQ(axe.is_weapon_range(),false);
+}
+
+TEST(Zoomorph_tests, default_constructor)
+{
+    Zoomorph Zolo;
+    EXPECT_EQ(Zolo.is_hiding(), false);
+    EXPECT_EQ(Zolo.is_imitating(), false);
+    EXPECT_EQ(Zolo.get_animal_power(), 0);
+}
+
+TEST(Zoomorph_tests, constructor)
+{
+    Weapon deathgun(300,3,true,false);
+    Zoomorph Zolo(deathgun,2000,200,100);
+    EXPECT_EQ(Zolo.is_hiding(), false);
+    EXPECT_EQ(Zolo.is_imitating(), false);
+    EXPECT_EQ(Zolo.get_animal_power(), 0);
+    EXPECT_EQ(Zolo.get_health(), 2000);
+    EXPECT_EQ(Zolo.get_size(), 200);
+    EXPECT_EQ(Zolo.get_strength(), 100);
+    EXPECT_EQ(Zolo.get_weapon_damage(), 300);
+    EXPECT_EQ(Zolo.get_weapon_cooldown(), 3);
+}
+
+TEST(Zoomorph_tests, set_methods)
+{
+    Zoomorph Zolo;
+    Zolo.hide();
+    EXPECT_EQ(Zolo.is_hiding(),true);
+    Zolo.imitate();
+    EXPECT_EQ(Zolo.is_imitating(),true);
+    Zolo.unhide();
+    EXPECT_EQ(Zolo.is_hiding(),false);
+    Zolo.unimitate();
+    EXPECT_EQ(Zolo.is_hiding(),false);
+    Zolo.set_animal_power(100);
+    EXPECT_EQ(Zolo.get_animal_power(),100);
+}
 
 int main()
 {
