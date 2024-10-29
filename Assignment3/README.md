a9071c675289c8e1239b4ee191a8fb4961dd03dd
Kirill Begaliev <st129977@student.spbu.ru>
2024-10-29 04:51:30 +0300

    FINALLY VERSION OF HOMEWORK Added last subclass Desipticons that can get rave and be strongly in it

--- Assignment3/README.md
+++ Assignment3/README.md
@@ -4,7 +4,7 @@ Begaliev Kirill Evgenievich, 24.B82-mm
 ## Contacts
 st129977@student.spbu.ru , tg: @silly_kat
 ## Description
-Transformers class with 2 subclasses: Insecticons(transform into insecticons) and Zoomorphes(transform into animals), every Transformer have weapon as aggregation and communication_module as composition
+Transformers class with 3 subclasses: **Desipticons**(evil Transforms), **Insecticons**(transform into insecticons) and **Zoomorphes**(transform into animals), every Transformer have *weapon* as aggregation and *communication_module* as composition
 ## Build
 "make" to build project, "make test" to build tests
 ## Run
--- Assignment3/test-transformer.cpp
+++ Assignment3/test-transformer.cpp
@@ -6,6 +6,7 @@ tests for all classes
 #include <iostream>
 #include "Zoomorph.hpp"
 #include "Insecticon.hpp"
+#include "Desipticon.hpp"
 
 TEST(Transformers_tests, default_constructor)
 {
@@ -127,6 +128,50 @@ TEST(Insecticon_tests, set_methods)
     Zolo.land();
     EXPECT_EQ(Zolo.is_in_the_air(), false);
     EXPECT_EQ(Zolo.get_chitin(), 100);
+}
+
+TEST(Desipticon_tests, default_constructor)
+{
+    Desipticon Zolo;
+    EXPECT_EQ(Zolo.get_primary_strength(), Zolo.get_strength());
+    EXPECT_EQ(Zolo.get_level_of_evil(), 25);
+    EXPECT_EQ(Zolo.get_rage(), 0);
+    EXPECT_EQ(Zolo.is_angry(), false);
+}
+
+TEST(Desipticon_tests, constructor)
+{
+    Weapon laser_sword(200,1,true,false);
+    Desipticon Zolo(laser_sword,2000,200,100);
+    EXPECT_EQ(Zolo.get_primary_strength(), Zolo.get_strength());
+    EXPECT_EQ(Zolo.get_level_of_evil(), 25);
+    EXPECT_EQ(Zolo.get_rage(), 0);
+    EXPECT_EQ(Zolo.is_angry(), false);
+    EXPECT_EQ(Zolo.get_health(), 2000);
+    EXPECT_EQ(Zolo.get_size(), 200);
+    EXPECT_EQ(Zolo.get_strength(), 100);
+    EXPECT_EQ(Zolo.get_weapon_damage(), 200);
+    EXPECT_EQ(Zolo.get_weapon_cooldown(), 1);
+}
+
+TEST(Desipticon_tests, set_methods)
+{
+    Desipticon Zolo;
+    Zolo.set_level_of_evil(50);
+    EXPECT_EQ(Zolo.get_level_of_evil(), 50);
+    Zolo.set_rage(80);
+    EXPECT_EQ(Zolo.get_rage(), 80);
+    Zolo.get_angry();
+    EXPECT_EQ(Zolo.is_in_rave(), false);
+    EXPECT_EQ(Zolo.is_angry(), true);
+    Zolo.get_angry();
+    EXPECT_EQ(Zolo.is_in_rave(), true);
+    EXPECT_EQ(Zolo.is_angry(), true);
+    EXPECT_EQ(Zolo.get_strength(), Zolo.get_primary_strength()*2);
+    Zolo.calm_down();
+    EXPECT_EQ(Zolo.is_in_rave(), false);
+    EXPECT_EQ(Zolo.is_angry(), false);
+    EXPECT_EQ(Zolo.get_strength(), Zolo.get_primary_strength());
 }
 
 int main()
